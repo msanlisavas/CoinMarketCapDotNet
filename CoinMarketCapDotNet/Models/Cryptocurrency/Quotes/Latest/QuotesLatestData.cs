ee570045b4926bb64975000d5e205eca9a649b5d
Murat Şanlısavaş <muratsanlisavas@gmail.com>
2023-07-04 14:48:28 +0300

    fix

--- CoinMarketCapDotNet/Models/Cryptocurrency/Quotes/Latest/QuotesLatestData.cs
+++ CoinMarketCapDotNet/Models/Cryptocurrency/Quotes/Latest/QuotesLatestData.cs
@@ -25,22 +25,22 @@ namespace CoinMarketCapDotNet.Models.Cryptocurrency.Quotes.Latest
         public int? IsFiat { get; set; }
 
         [JsonProperty("cmc_rank")]
-        public int CmcRank { get; set; }
+        public int? CmcRank { get; set; }
 
         [JsonProperty("num_market_pairs")]
-        public int NumMarketPairs { get; set; }
+        public int? NumMarketPairs { get; set; }
 
         [JsonProperty("circulating_supply")]
-        public double CirculatingSupply { get; set; }
+        public double? CirculatingSupply { get; set; }
 
         [JsonProperty("total_supply")]
-        public double TotalSupply { get; set; }
+        public double? TotalSupply { get; set; }
 
         [JsonProperty("market_cap_by_total_supply")]
-        public double MarketCapByTotalSupply { get; set; }
+        public double? MarketCapByTotalSupply { get; set; }
 
         [JsonProperty("max_supply")]
-        public double MaxSupply { get; set; }
+        public double? MaxSupply { get; set; }
 
         [JsonProperty("date_added")]
         public string DateAdded { get; set; }
