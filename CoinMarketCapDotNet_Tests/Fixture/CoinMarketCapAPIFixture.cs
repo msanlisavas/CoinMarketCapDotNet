907d01cdd531a1a49da1404a5340d1d95b297ea1
Murat Şanlısavaş <muratsanlisavas@gmail.com>
2023-07-04 14:31:20 +0300

    Revert "staged"
    This reverts commit e75304e9e823de7861346d7d50d88416ac4d058c.

--- CoinMarketCapDotNet_Tests/Fixture/CoinMarketCapAPIFixture.cs
+++ CoinMarketCapDotNet_Tests/Fixture/CoinMarketCapAPIFixture.cs
@@ -1,9 +1,12 @@
-﻿namespace CoinMarketCapDotNet_Tests.Collection
+﻿using CoinMarketCapDotNet.Api;
+using System;
+
+namespace CoinMarketCapDotNet_Tests.Collection
 {
     public class CoinMarketCapAPIFixture : IDisposable
     {
         public CoinMarketCapAPI CoinMarketCapAPI { get; private set; }
-        private readonly string _apiKey = "e2bd3ea8-329a-4575-8ca2-6cb67d919618";
+        private readonly string _apiKey = "your-valid-api-key";
 
         public CoinMarketCapAPIFixture()
         {
